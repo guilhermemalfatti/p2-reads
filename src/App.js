a8bb5b8e08b843eec345faa5f099b22e90233af6
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-10-31 18:47:42 -0300

    feat(layout) Add post layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/header/header.js'
+import Header from './components/header/header'
+import PostsList from './components/postsList/postsList'
+import PostsPage from './components/postPage/postPage'
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Header/>
+      <div>
+        <Header/>
+        <Route exact path={process.env.PUBLIC_URL + '/post'} render={() => (
+          <PostsPage/>
+        )}
+        />
+        <Route exact path={process.env.PUBLIC_URL + '/'} render={() => (
+          <PostsList/>
+        )}
+        />
+
+      </div>
+
     );
   }
 }
---
+++ src/components/postPage/postPage.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class PostsPage extends Component {
+
+    render() {
+        return (
+            <div className="container">
+            <div>
+                <div id="post">
+                    <header>
+                        <img src={"https://cdn1.iconfinder.com/data/icons/flat-business-icons/128/user-64.png"}/>
+                        <p><a href="#" className="name">Daniel Svensson</a> uploaded a new photo to the album <a href="#">Beautiful
+                                Nature</a>.<span>20.04.2015 @ 12:00 PM</span></p>
+                        <div className="option"></div>
+                    </header>
+
+                    <div className="content">
+                        <p>Nautre’s beauty can’t be described in words.</p>
+                        <p>Nautre’s beauty can’t be described in words.</p>
+                        <p>Nautre’s beauty can’t be described in words.</p>
+                        <p>Nautre’s beauty can’t be described in words.</p>
+                        <p>Nautre’s beauty can’t be described in words.</p>
+                        <div id="social">
+                            <div className="post-comments">402</div>
+                        </div>
+                    </div>
+
+                    <div className="hide-comments">
+                        <input type="checkbox" value="" id="hide-button" name="check" checked />
+                        <label for="hide-button" className="button">Hide Comments</label>
+                    </div>
+
+                    <div className="comments">
+                        <ul>
+                            <li key="1">
+                                <div className="user-comment">
+                                    <img src={"https://cdn1.iconfinder.com/data/icons/flat-business-icons/128/user-32.png"}/>
+                                    <header><a href="#" className="name">Josephine Bowler</a><span>20.02.2015 @ 09:01</span></header>
+                                    <div className="content">
+                                        <p>This picture is absolutely amazing! You are such a good photographer :)</p>
+                                    </div>
+                                    <a href="" className="reply">Reply</a>
+                                </div>
+                            </li>
+
+
+
+                            <li key="2">
+                                <div className="user-comment">
+                                    <img src={"https://cdn1.iconfinder.com/data/icons/flat-business-icons/128/user-32.png"}/>
+                                    <header><a href="#" className="name">Jennifer Lee</a><span>21.02.2015 @ 14:00</span></header>
+                                    <div className="content">
+                                        <p>Wow!!! Cool shot, Daniel! You should post more often =)</p>
+                                    </div>
+                                    <a href="" className="reply">Reply</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default PostsPage
