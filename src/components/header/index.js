ef58854a111607484a850933daca57accc6f4c57
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-11-08 23:06:02 -0200

    fix(nav) Category navigation
    #001

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -51,11 +51,11 @@ class Header extends Component {
                         {categories && categories.map((cat) => (
 
                             category === cat.path ?
-                                <Link key={cat.path} to={cat.path} >
+                                <Link key={cat.path} to={'/'.concat(cat.path)} >
                                     <span className="active" onClick={()=>{this.filter(cat.path)}}> {cat.name} </span>
                                 </Link>
                                 :
-                                <Link key={cat.path} to={cat.path} >
+                                <Link key={cat.path} to={'/'.concat(cat.path)} >
                                     <span onClick={()=>{this.filter(cat.path)}}> {cat.name} </span>
                                 </Link>
 
