17072bf5b3b78163bed438746136b0c68257c273
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-11-16 17:34:53 -0200

    Updates

---
+++ precache-manifest.6211cee23d25b0d5a0b5a536b243e897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c73fdcacff712a2822c9",
+    "url": "/p2-reads/static/js/runtime~main.c73fdcac.js"
+  },
+  {
+    "revision": "3bb54c7d00d6fd259397",
+    "url": "/p2-reads/static/js/main.3bb54c7d.chunk.js"
+  },
+  {
+    "revision": "2472ce9a1e6c1b97480a",
+    "url": "/p2-reads/static/js/1.2472ce9a.chunk.js"
+  },
+  {
+    "revision": "3bb54c7d00d6fd259397",
+    "url": "/p2-reads/static/css/main.005d63a0.chunk.css"
+  },
+  {
+    "revision": "d0f4988e90016b16ad47c9f70977d2d5",
+    "url": "/p2-reads/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a579bcb76e8bd8836ae59d5bc7bb1f4.js"
+  "/p2-reads/precache-manifest.6211cee23d25b0d5a0b5a536b243e897.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/p2-reads/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
