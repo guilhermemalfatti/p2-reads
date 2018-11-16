eaf0cff98c17ebebe4d48125638d5423b892c840
Guilherme Malfatti <guimalfatti76@gmail.com>
2018-11-16 17:28:55 -0200

    fix(route) Update basename

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const store = createStore(reducer,
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>,
